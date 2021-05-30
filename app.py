10354257176e25edad14d57eb01adc24c92aa872
Aafaq Altaf <70970247+Reshi-aafaq@users.noreply.github.com>
2021-05-30 17:33:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ loginmanager = LoginManager(app)
 def load_user(user_id):
     User.query.get(int(user_id))
 class User(db.Model,UserMixin):
+    __tablename__ = 'mytable'
     id = db.Column(db.Integer , primary_key = True)
     username = db.Column(db.String(20),nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
